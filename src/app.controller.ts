afbd923af461f06e456275b0b620f929252d3608
Sergio Peralta <seperalta88@gmail.com>
2023-09-09 19:20:45 -0300

    add about controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,19 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get('/')
   @Render('index')
-  index() {}
+  index() {
+    return {
+      title: 'Home Page - Online Store',
+    };
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    const viewData = [];
+    viewData['title'] = 'About Page - Online Store';
+    viewData['author'] = 'Developed by: Your Name';
+    const data1 = 'About us - Online Store';
+    return { title: data1, subtitle: 'About us', viewData: viewData };
+  }
 }
