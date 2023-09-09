10449bbab50da0aa15adadb72800ddb6f7d4c425
Sergio Peralta <seperalta88@gmail.com>
2023-09-09 15:36:46 -0300

    Add handlebars

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as hbs from 'hbs';
+import * as hbsU from 'hbs-utils';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function main() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
+  hbsU.registerWatchedPartials(join(__dirname, '..', 'views/layouts'));
   await app.listen(3000);
 }
-bootstrap();
+main();
